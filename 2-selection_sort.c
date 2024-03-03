2c3f650675a26d6c57dfa839192b157916a04625
dohaMST <mastourdoha.dev001@gmail.com>
2024-03-03 12:40:37 -0800

    task2f1

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,29 +3,33 @@
 /**
  * selection_sort - a function that sorts an array
  * @array: the array we wanna sort
- * @size: the size of the array
+ * @size: the size of array
  */
 
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j;
-	int x;
+	unsigned int i, j, idx;
+	int x, min;
 
-	if (array == NULL || size < 2)
-		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		min = array[i + 1];
+		idx = i + 1;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
+			if (min > array[j])
 			{
-				x = array[i];
-				array[i] = array[j];
-				array[j] = x;
-				/*print_array(array, size);*/
+				min = array[j];
+				idx = j;
 			}
-			/*print_array(array, size);*/
 		}
-		/*print_array(array, size);*/
+
+		if (array[i] > min)
+		{
+			x = array[i];
+			array[i] = array[idx];
+			array[idx] = x;
+			print_array(array, size);
+		}
 	}
 }
---
+++ 2-task.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - a function that sorts an array
+ * @array: the array we wanna sort
+ * @size: the size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	int x;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				x = array[i];
+				array[i] = array[j];
+				array[j] = x;
+				/*print_array(array, size);*/
+			}
+			/*print_array(array, size);*/
+		}
+		/*print_array(array, size);*/
+	}
+}
