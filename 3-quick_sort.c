e029329f09ba32bf5ae6dd9d1e248d6a3ef36dd6
dohaMST <mastourdoha.dev001@gmail.com>
2024-03-04 17:45:55 -0800

    task3f1

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,78 +1,95 @@
 #include "sort.h"
-/**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
-void swap(int *array, ssize_t item1, ssize_t item2)
-{
-	int tmp;
 
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
-}
+void quick_sort(int *array, size_t size);
+void q_sort(int *array, ssize_t start, ssize_t end, int size);
+void swap(int *array, ssize_t i, ssize_t j);
+int partition_func(int *array, ssize_t start, ssize_t end, size_t size);
+
+/*quick_sort function*/
+
 /**
- *lomuto_partition - lomuto partition sorting scheme implementation
- *@array: array
- *@first: first array element
- *@last: last array element
- *@size: size array
- *Return: return the position of the last element sorted
+ *quick_sort - quick sort algorithm
+ *@array: the array we wanna sort
+ *@size: the size of array
  */
-int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size)
-{
-	int pivot = array[last];
-	ssize_t current = first, finder;
 
-	for (finder = first; finder < last; finder++)
-	{
-		if (array[finder] < pivot)
-		{
-			if (array[current] != array[finder])
-			{
-				swap(array, current, finder);
-				print_array(array, size);
-			}
-			current++;
-		}
-	}
-	if (array[current] != array[last])
-	{
-		swap(array, current, last);
-		print_array(array, size);
-	}
-	return (current);
-}
-/**
- *qs - qucksort algorithm implementation
- *@array: array
- *@first: first array element
- *@last: last array element
- *@size: array size
- */
-void qs(int *array, ssize_t first, ssize_t last, int size)
-{
-	ssize_t position = 0;
-
-
-	if (first < last)
-	{
-		position = lomuto_partition(array, first, last, size);
-
-		qs(array, first, position - 1, size);
-		qs(array, position + 1, last, size);
-	}
-}
-/**
- *quick_sort - prepare the terrain to quicksort algorithm
- *@array: array
- *@size: array size
- */
 void quick_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
 		return;
-	qs(array, 0, size - 1, size);
+	q_sort(array, 0, size - 1, size);
+}
+
+/*q_sort function*/
+
+/**
+ *q_sort - quick sort algorithm
+ *@array: the array we wanna sort
+ *@start: the start
+ *@end: the end
+ *@size: the size of the array
+ */
+void q_sort(int *array, ssize_t start, ssize_t end, int size)
+{
+	ssize_t x = 0;
+
+
+	if (start < end)
+	{
+		x = partition_func(array, start, end, size);
+
+		q_sort(array, start, x - 1, size);
+		q_sort(array, x + 1, end, size);
+	}
+}
+/*swap function */
+
+/**
+*swap - a function that swaps two elements
+*@array: array we wanna sort
+*@i: elm 1
+*@j: elm 2
+*/
+void swap(int *array, ssize_t i, ssize_t j)
+{
+	int x;
+
+	x = array[i];
+	array[i] = array[j];
+	array[j] = x;
+}
+
+/*partition_func*/
+
+/**
+ *partition_func - quick sort algo
+ *@array: array we wanna sort
+ *@start: the start
+ *@end: the end
+ *@size: the size of array
+ *Return: return the position for the new pivot
+ */
+int partition_func(int *array, ssize_t start, ssize_t end, size_t size)
+{
+	int pv = array[end];
+	ssize_t nw_pv = start, x;
+
+	for (x = start; x < end; x++)
+	{
+		if (array[x] < pv)
+		{
+			if (array[nw_pv] != array[x])
+			{
+				swap(array, nw_pv, x);
+				print_array(array, size);
+			}
+			nw_pv++;
+		}
+	}
+	if (array[nw_pv] != array[end])
+	{
+		swap(array, nw_pv, end);
+		print_array(array, size);
+	}
+	return (nw_pv);
 }
