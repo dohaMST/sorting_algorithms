a1c84bd5f53ce3442ff48e2234a86c207e9c2cfa
dohaMST <mastourdoha.dev001@gmail.com>
2024-03-04 16:46:04 -0800

    task3v2

--- sort.h
+++ sort.h
@@ -27,16 +27,5 @@ void insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int *num1, int *num2);
-int partition(int arr[], int first, int last, size_t size);
-void quick_sort1(int *array, int first, int last, size_t size);
-void quick_sort_hoare1(int *array, int first, int last, size_t size);
-void shell_sort(int *array, size_t size);
-listint_t *get_last_node(listint_t *list);
-listint_t *swap_list(listint_t *list, listint_t *curr2);
-void cocktail_sort_list(listint_t **list);
-int calc_list_size(listint_t *list);
-void counting_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
 
 #endif
